ca02a2f556fa82a8c82944b87946d66e92b3466b
Harvey Dent <jakubwarumzer@gmail.com>
2018-03-28 00:49:01 +0200

    Adjusted display for new concept; Area matrix fix

--- maze.py
+++ maze.py
@@ -1,16 +1,17 @@
 from enum import Enum
 import random
 
+
 class Border(Enum):
     PASSAGE = 0
     WALL = 1
 
 class Cell:
     def __init__(self):
-        self.borders = {'N': Border.Wall,
-                        'E': Border.Wall,
-                        'W': Border.Wall,
-                        'S': Border.Wall}
+        self.borders = {'N': Border.WALL,
+                        'E': Border.WALL,
+                        'W': Border.WALL,
+                        'S': Border.WALL}
 
     def carve_passage(self, direction):
         if direction not in self.borders:
@@ -35,29 +36,23 @@ class Maze:
 
     def generate(self):
         #At first, we fill labirynth with walls
-        self.area = [[Cell()]*self.width for i in range(self.height)]
+        self.area = [[Cell() for i in range(self.width)] for i in range(self.height)]
         
-        #Then, we pick Entry and Exit
-        self.entry = 0, random.randint(1, self.width - 2)
-        self.exit = self.width - 1, random.randint(1, self.width - 2)
-
-        self.create_passage(self.entry)
-        self.create_passage(self.exit)
-
-        #Now, we can create random web of passage between Entry and Exit
-
-    def check_if_border_wall(self, point):
-        if point[0] == 0 or point[0] == self.height - 1: return True
-        if point[1] == 0 or point[1] == self.width - 1: return True
-
-        return False
-
-
     def create_passage(self, point):
-        self.area[point[0]][point[1]] = SpotType.PASSAGE
+        self.area[point[0]][point[1]] = Border.PASSAGE
 
     def display(self):
+        #Upper border, always solid
+        for i in range(self.width):
+            print(" _", end='')
+        
+        
+        #Then we display borders for each cell
         for i in range(self.height):
+            print('')
+            
             for j in range(self.width):
-                print(self.area[i][j].value, end='') #'end' argument ensures that there is no newline printed
-            print('\n')
+                print('|', end='') if self.area[i][j].borders['W'] == Border.WALL else print(' ', end='')
+                print('_', end='') if self.area[i][j].borders['S'] == Border.WALL else print(' ', end='')
+
+                if j == self.width - 1: print('|', end='')         
